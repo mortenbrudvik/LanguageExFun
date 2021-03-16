33bd86cd5f38ee8a95013dc28c61800a7f4c3b32
Morten Brudvik <morten@brudvik.com>
2021-03-16 18:20:25 +0100

    First commit - testing out LanguageEX library

---
+++ FunConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
