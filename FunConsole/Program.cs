89f4272d5a95e1d8b9a3a60d282607c90480f99f
Morten Brudvik <morten@brudvik.com>
2021-03-16 23:13:16 +0100

    partial application, currying

--- FunConsole/Program.cs
+++ FunConsole/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Linq;
+using LanguageExt;
+using static System.Console;
+
+using static LanguageExt.Prelude;
 
 namespace FunConsole
 {
+    /// <summary>
+    /// https://github.com/louthy/language-ext
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new[] {"Bob", "Lisa", "Bill", "Joe"};
+
+            Func<string, string, string> greet = (greeting, name) => $"{greeting} {name}!";
+            WriteLine("Greet Normal");
+            names.Map(x=>greet("Hello", x)).ToList().ForEach(WriteLine);
+
+            WriteLine("\nGreet with partial applications");
+            var greetSimple = par(greet, "Hi");
+            names.Map(x=>greetSimple(x)).ToList().ForEach(WriteLine);
+
+            WriteLine("\nGreet with partial application, currying");
+            var greetCowboy = curry(greet)("Howdy");
+            names.Map(x=>greetCowboy(x)).ToList().ForEach(WriteLine);
+
+            WriteLine("\nAdd method, currying");
+
+            var add = curry((int x, int y) => x + y);
+            var res = add(2)(2);
+            WriteLine("2+2 = " + res);
+
+
+
+            ReadLine();
+
         }
     }
 }
