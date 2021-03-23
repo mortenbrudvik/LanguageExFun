783c431037a21b9c554f87363ed5bd16ced56a64
Morten Brudvik <morten@brudvik.com>
2021-03-23 08:48:49 +0100

    monad examples

--- FunConsole/Program.cs
+++ FunConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LanguageExt;
 using static System.Console;
@@ -15,6 +16,18 @@ namespace FunConsole
         static void Main(string[] args)
         {
             PartialApplicationAndCurrying.Run();
+            
+            // Nomad examples
+
+            // Bind multiple parameters
+            static Option<int> Multiply(int x, int y) => Some(x * y);
+            static Option<int> MultiplicationWithBind(string strX, string strY)
+                => parseInt(strX).Bind(x => parseInt(strY)
+                    .Bind(y => Multiply(x,y)));
+            var multiplicationResult = MultiplicationWithBind("4", "2");
+            Out.WriteLine("4*2 = " + multiplicationResult);
+            
+
 
             ReadLine();
 
