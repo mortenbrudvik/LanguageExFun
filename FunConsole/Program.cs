20b6fe7273370c00f1f477eefe618e592d6712ce
Morten Brudvik <morten@brudvik.com>
2021-03-31 18:35:59 +0200

    validation chaining

--- FunConsole/Program.cs
+++ FunConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
 using DomainModel;
 using LanguageExt;
 using LanguageExt.Common;
@@ -28,28 +30,52 @@ namespace FunConsole
 
             DataFunctionality.Run();
 
-            Func<int, Validation<CustomError, int>> isPositive = number =>
+            Func<int, Validation<CustomError, int>> validatePositive = number =>
                 number switch
                 {
                     > 0 => Success<CustomError, int>(number),
                     _ => Fail<CustomError, int>(CustomError.New("Not positive number!"))
                 };
+            Func<int, Validation<CustomError, int>> validateOdd = number =>
+                (number%2==0) switch
+                {
+                    false => Success<CustomError, int>(number),
+                    _ => Fail<CustomError, int>(CustomError.New("Not odd number!"))
+                };
+            Func<int, Validation<CustomError, int>> validateEven = number =>
+                (number%2==0) switch
+                {
+                    true => Success<CustomError, int>(number),
+                    _ => Fail<CustomError, int>(CustomError.New("Not even number!"))
+                };
             
             var isAllPositive = Range(1, 10)
-                .Map(isPositive)
+                .Map(validatePositive)
                 .All(x => x.IsSuccess);
             Out.WriteLine($"All Positive (1..10): {isAllPositive}" );
 
             isAllPositive = Range(-1, 12)
-                .Map(isPositive)
+                .Map(validatePositive)
                 .All(x=>x.IsSuccess);
             Out.WriteLine($"All Positive (-1..10): {isAllPositive}" );
 
             var totalPositive = Range(-1, 12)
-                .Map(isPositive)
+                .Map(validatePositive)
                 .Bind(x => x)
                 .Sum(x=>x.Success);
             Out.WriteLine($"Sum of positive number (-1..10): {totalPositive}" );
+
+            validatePositive(2)
+                .Bind(validateOdd)
+                .Match(x =>
+                {
+                    WriteLine("Success: " + x);
+                    return x;
+                }, errors =>
+                {
+                    errors.ToList().ForEach(err => WriteLine("Error: " + err.Value));
+                    return 0;
+                });
             
             ReadLine();
         }
