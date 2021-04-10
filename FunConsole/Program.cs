7e9c37349419f756c187f415a4653b1fc67dcc92
Morten Brudvik <morten@brudvik.com>
2021-04-10 09:33:49 +0200

    moved code into a separate class

--- FunConsole/Program.cs
+++ FunConsole/Program.cs
@@ -30,65 +30,12 @@ namespace FunConsole
             MultiArgumentFunctions.Run();
 
             DataFunctionality.Run();
-
-            Func<int, Validation<CustomError, int>> validatePositive = number =>
-                number switch
-                {
-                    > 0 => Success<CustomError, int>(number),
-                    _ => Fail<CustomError, int>(CustomError.New("Not positive number!"))
-                };
-            Func<int, Validation<CustomError, int>> validateOdd = number =>
-                (number%2==0) switch
-                {
-                    false => Success<CustomError, int>(number),
-                    _ => Fail<CustomError, int>(CustomError.New("Not odd number!"))
-                };
-            Func<int, Validation<CustomError, int>> validateEven = number =>
-                (number%2==0) switch
-                {
-                    true => Success<CustomError, int>(number),
-                    _ => Fail<CustomError, int>(CustomError.New("Not even number!"))
-                };
             
-            var isAllPositive = Range(1, 10)
-                .Map(validatePositive)
-                .All(x => x.IsSuccess);
-            Out.WriteLine($"All Positive (1..10): {isAllPositive}" );
+            ValidationFun.Run();
 
-            isAllPositive = Range(-1, 12)
-                .Map(validatePositive)
-                .All(x=>x.IsSuccess);
-            Out.WriteLine($"All Positive (-1..10): {isAllPositive}" );
-
-            var totalPositive = Range(-1, 12)
-                .Map(validatePositive)
-                .Bind(x => x)
-                .Sum(x=>x.Success);
-            Out.WriteLine($"Sum of positive number (-1..10): {totalPositive}" );
-
-            validatePositive(1)
-                .Do(result => WriteLine($"Number {result} is positive"))
-                .Bind(validateOdd)
-                .Do(result => WriteLine($"Number {result} is positive and odd"))
-                .Match(x =>
-                {
-                    WriteLine("Validation success");
-                    return x;
-                }, errors =>
-                {
-                    WriteLine("Validation failed");
-                    errors.ToList().ForEach(err => WriteLine("Error: " + err.Value));
-                    return 0;
-                });
-            
             ReadLine();
         }
 
-        public class CustomError : NewType<CustomError, string>
-        {
-            public CustomError(string error) : base(error)
-            {
-            }
-        }
+
     }
 }
---
+++ FunConsole/ValidationFun.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using DomainModel;
+using LanguageExt;
+using LanguageExt.ClassInstances;
+using LanguageExt.Common;
+using Microsoft.FSharp.Collections;
+using static System.Console;
+using static System.Decimal;
+using static System.Linq.Enumerable;
+using static LanguageExt.Prelude;
+using Error = LanguageExt.Common.Error;
+
+namespace FunConsole
+{
+    public static class ValidationFun
+    {
+        public static void Run()
+        {
+            Func<int, Validation<CustomError, int>> validatePositive = number =>
+                number switch
+                {
+                    > 0 => Success<CustomError, int>(number),
+                    _ => Fail<CustomError, int>(CustomError.New("Not positive number!"))
+                };
+            Func<int, Validation<CustomError, int>> validateOdd = number =>
+                (number%2==0) switch
+                {
+                    false => Success<CustomError, int>(number),
+                    _ => Fail<CustomError, int>(CustomError.New("Not odd number!"))
+                };
+            Func<int, Validation<CustomError, int>> validateEven = number =>
+                (number%2==0) switch
+                {
+                    true => Success<CustomError, int>(number),
+                    _ => Fail<CustomError, int>(CustomError.New("Not even number!"))
+                };
+            
+            var isAllPositive = Range(1, 10)
+                .Map(validatePositive)
+                .All(x => x.IsSuccess);
+            Out.WriteLine($"All Positive (1..10): {isAllPositive}" );
+
+            isAllPositive = Range(-1, 12)
+                .Map(validatePositive)
+                .All(x=>x.IsSuccess);
+            Out.WriteLine($"All Positive (-1..10): {isAllPositive}" );
+
+            var totalPositive = Range(-1, 12)
+                .Map(validatePositive)
+                .Bind(x => x)
+                .Sum(x=>x.Success);
+            Out.WriteLine($"Sum of positive number (-1..10): {totalPositive}" );
+
+            validatePositive(1)
+                .Do(result => WriteLine($"Number {result} is positive"))
+                .Bind(validateOdd)
+                .Do(result => WriteLine($"Number {result} is positive and odd"))
+                .Match(x =>
+                {
+                    WriteLine("Validation success");
+                    return x;
+                }, errors =>
+                {
+                    WriteLine("Validation failed");
+                    errors.ToList().ForEach(err => WriteLine("Error: " + err.Value));
+                    return 0;
+                });
+        }
+        
+        public class CustomError : NewType<CustomError, string>
+        {
+            public CustomError(string error) : base(error)
+            {
+            }
+        }
+    }
+}
