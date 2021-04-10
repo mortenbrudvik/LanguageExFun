6b3bdde9bf86bcc4c9ac5326381fb2e2302f3082
Morten Brudvik <morten@brudvik.com>
2021-04-10 09:34:26 +0200

    moved code into a separate class

--- FunConsole/ValidationFun.cs
+++ FunConsole/ValidationFun.cs
@@ -1,18 +1,9 @@
 using System;
 using System.Linq;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Security.Cryptography.X509Certificates;
-using DomainModel;
 using LanguageExt;
-using LanguageExt.ClassInstances;
-using LanguageExt.Common;
-using Microsoft.FSharp.Collections;
 using static System.Console;
-using static System.Decimal;
 using static System.Linq.Enumerable;
 using static LanguageExt.Prelude;
-using Error = LanguageExt.Common.Error;
 
 namespace FunConsole
 {
