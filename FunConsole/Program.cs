03f15ddc73e49684cdbd4f12a41263aa055bc427
Morten Brudvik <morten@brudvik.com>
2021-04-14 08:22:03 +0200

    Lazy API for working with Option. OrElse method will not evaluate the next method if the first one succeed.

--- FunConsole/Program.cs
+++ FunConsole/Program.cs
@@ -32,10 +32,72 @@ namespace FunConsole
             DataFunctionality.Run();
             
             ValidationFun.Run();
+            
+            // Lazy Computation
+            var volvoFactory = new CarFactory("Volvo");
+            var toyotaFactory = new CarFactory("Toyota");
+            volvoFactory.ProduceCars(2, "red");
+            toyotaFactory.ProduceCars(2, "blue");
+
+            volvoFactory.BuyCar("red")
+                .OrElse(toyotaFactory.BuyCar("blue")) // Not lazy - both will be called - two cars bought
+                .Match(
+                    car => WriteLine($"Bought {car.Brand} with color {car.Color}"),
+                    () => WriteLine("No matching car"));
+
+            WriteLine($"Volvo factory have {volvoFactory.Cars.Count} red cars for sale");
+            WriteLine($"Toyota factory have {volvoFactory.Cars.Count} blue cars for sale");
+
+            volvoFactory.BuyCar("red")
+                .OrElse(() => toyotaFactory.BuyCar("blue")) // () => Lazy, the second will not be evaluated - only one car bought
+                .Match(
+                    car => WriteLine($"Bought {car.Brand} with color {car.Color}"),
+                    () => WriteLine("No matching car"));
+            
+            WriteLine($"Volvo factory have {volvoFactory.Cars.Count} red cars for sale");
+            WriteLine($"Toyota factory have {toyotaFactory.Cars.Count} blue cars for sale");
 
             ReadLine();
         }
 
+        private static Option<Car> NoCar => None;
+        private static Option<Car> VolvoRed => Some(new Car("Volvo", "red"));
+    }
+    
+    public static class OptionExt
+    {
+        public static Option<T> OrElse<T>(this Option<T> left, Option<T> right)
+            => left.Match( 
+                () => right,
+                _ => left);
 
+        public static Option<T> OrElse<T>(this Option<T> left, Func<Option<T>> right)
+            => left.Match(
+                right, 
+                _ => left);
+    }
+
+    internal record Car(string Brand, string Color);
+
+    internal class CarFactory
+    {
+        private readonly string _brand;
+        readonly List<Car> _cars = new();
+
+        public CarFactory(string brand) => _brand = brand;
+        public IReadOnlyCollection<Car> Cars => _cars;
+
+        public void ProduceCars(int number, string withColor)
+        {
+            _cars.AddRange(Repeat(_brand, number)
+                .Select(brand => new Car(brand, withColor)));
+        }
+
+        public Option<Car> BuyCar(string withColor)
+            => _cars.Find(x => x.Color == withColor) switch
+            {
+                { } car => _cars.Remove(car) ? car : None,
+                _ => None
+            };
     }
 }
